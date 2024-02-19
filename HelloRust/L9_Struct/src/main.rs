17685198b13b631048760630dba3955eb3df347a
CoffeeKiller <18320210361@163.com>
2024-02-19 21:30:41 +0800

    update lession9

--- HelloRust/L9_Struct/src/main.rs
+++ HelloRust/L9_Struct/src/main.rs
@@ -62,6 +62,26 @@ fn main() {
     println!("rect1 is {:?}", rect1); // 此处直接运行会产生错误: `Rectangle` doesn't implement `std::fmt::Display`
     // 此处需要使用 {:?} 占位符
 
+
+    // 然后在 main 中将我们先前调用 area 方法并传递 rect1 作为参数的地方，
+    // 改成使用 方法语法（method syntax）在 Rectangle 实例上调用 area 方法。
+    // 方法语法获取一个实例并加上一个点号，后跟方法名、圆括号以及任何参数。
+    let rect1 = Rectangle { width: 30, height: 50 };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+
+    let sq = Rectangle::square(3);
+
 }
 
 // 将此行代码加入后就不会出现: `Rectangle` doesn't implement `std::fmt::Display` 错误了
@@ -69,6 +89,23 @@ fn main() {
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn area(width: u32, height: u32) -> u32 {
@@ -117,4 +154,65 @@ fn area2(rectangle: &Rectangle) -> u32 {
             它们被称为"类单元结构体"（unit-like structs）因为它们类似于 ()，即 unit 类型。
             类单元结构体常常在你想要在某个类型上实现 trait 但不需要在类型中存储数据的时候发挥作用。
 
+
+        方法
+            方法与函数类似：它们使用 fn 关键字和名称声明，可以拥有参数和返回值，同时包含在某处调用该方法时会执行的代码。
+            不过方法与函数是不同的，因为它们在结构体的上下文中被定义（或者是枚举或 trait 对象的上下文). 
+            并且它们第一个参数总是 self，它代表调用该方法的结构体实例。
+
+
+            定义方法
+                impl Rectangle {
+                    fn area(&self) -> u32 {
+                        self.width * self.height
+                    }
+                }
+                
+                为了使函数定义于 Rectangle 的上下文中，我们开始了一个 impl 块（impl 是 implementation 的缩写）。
+                接着将 area 函数移动到 impl 大括号中，并将签名中的第一个（在这里也是唯一一个）参数和函数体中其他地方的对应参数改成 self。
+
+                在 area 的签名中，使用 &self 来替代 rectangle: &Rectangle，
+                因为该方法位于 impl Rectangle 上下文中所以 Rust 知道 self 的类型是 Rectangle。
+                注意仍然需要在 self 前面加上 &，就像 &Rectangle 一样。方法可以选择获取 self 的所有权，
+                或者像我们这里一样不可变地借用 self，或者可变地借用 self，就跟其他参数一样。
+                
+
+                这里选择 &self 的理由跟在函数版本中使用 &Rectangle 是相同的：我们并不想获取所有权，只希望能够读取结构体中的数据，而不是写入。
+                如果想要在方法中改变调用方法的实例，需要将第一个参数改为 &mut self。
+                通过仅仅使用 self 作为第一个参数来使方法获取实例的所有权是很少见的；
+                这种技术通常用在当方法将 self 转换成别的实例的时候，这时我们想要防止调用者在转换之后使用原始的实例。
+
+
+                使用方法替代函数，除了可使用方法语法和不需要在每个函数签名中重复 self 的类型之外，其主要好处在于组织性。
+                我们将某个类型实例能做的所有事情都一起放入 impl 块中，而不是让将来的用户在我们的库中到处寻找 Rectangle 的功能。
+
+
+                关于 -> 运算符到哪去了？
+                    在 C/C++ 语言中，有两个不同的运算符来调用方法：. 直接在对象上调用方法，
+                    而 -> 在一个对象的指针上调用方法，这时需要先解引用（dereference）指针。
+                    换句话说，如果 object 是一个指针，那么 object->something() 就像 (*object).something() 一样。
+
+                    Rust 并没有一个与 -> 等效的运算符；相反，Rust 有一个叫 自动引用和解引用（automatic referencing and dereferencing）的功能。
+                    方法调用是 Rust 中少数几个拥有这种行为的地方。
+                    
+                    他是这样工作的：当使用 object.something() 调用方法时，Rust 会自动为 object 添加 &、&mut 或 * 以便使 object 与方法签名匹配。
+                    也就是说，这些代码是等价的：
+                        p1.distance(&p2);
+                        (&p1).distance(&p2);
+
+                    第一行看起来简洁的多。
+                    这种自动引用的行为之所以有效，是因为方法有一个明确的接收者———— self 的类型。
+                    在给出接收者和方法名的前提下，Rust 可以明确地计算出方法是仅仅读取（&self），做出修改（&mut self）或者是获取所有权（self）。
+                    事实上，Rust 对方法接收者的隐式借用让所有权在实践中更友好。
+
+
+                关联函数
+                    impl 块的另一个有用的功能是：允许在 impl 块中定义 不 以 self 作为参数的函数。
+                    这被称为 关联函数（associated functions），因为它们与结构体相关联。
+                    它们仍是函数而不是方法，因为它们并不作用于一个结构体的实例。
+                    你已经使用过 String::from 关联函数了。
+
+                
+                多个 impl 块
+                    每个结构体都允许拥有多个 impl 块。
 */
